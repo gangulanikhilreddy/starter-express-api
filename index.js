3e20821e6d028fc32ad9cd0c4e22f6144d90d300
gangulanikhilreddy <74185262+gangulanikhilreddy@users.noreply.github.com>
2023-02-16 00:15:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
+
+const cron = require("node-cron");
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+let task = (val) => cron.schedule(val, function () {
+    console.log(val)
+    console.log("running a task every 10 second");
+});
+task('1 * * * * *')
+task('0 30 18 * * *')
 app.listen(process.env.PORT || 3000)
